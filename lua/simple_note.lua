bf9027170c6b21e24aea9fe8ea8f0ecfa41c05da
Robert Moelker <rmoelker@gmail.com>
2024-01-28 22:57:41 +0100

    fix: Make sure it doesn't crash without args

--- lua/simple_note.lua
+++ lua/simple_note.lua
@@ -36,6 +36,7 @@ M.listNotes = function()
       map({ "i", "n" }, "<C-n>", function(prompt_bufnr)
         local full_path = M.createNoteFile()
         local picker = actions_state.get_current_picker(prompt_bufnr)
+
         vim.notify(full_path .. " has been created")
         picker:refresh(finders.new_oneshot_job(find_command, { cwd = M.config.notes_dir }), { reset_prompt = false })
       end)
@@ -44,6 +45,7 @@ M.listNotes = function()
         local filePath = vim.fn.expand(M.config.notes_dir) .. entry.value
         local picker = actions_state.get_current_picker(prompt_bufnr)
         local confirm = vim.fn.confirm("Are you sure you want to delete " .. filePath .. "?", "&Yes\n&No")
+
         if confirm == 1 then
           os.remove(filePath)
           vim.notify(filePath .. " has been deleted")
@@ -76,7 +78,7 @@ M.createNoteFile = function(opts)
   local notes_path = vim.fn.expand(M.config.notes_dir)
   local full_path = notes_path
 
-  if (opts.fargs[1]) then
+  if (opts ~= nil and opts.fargs[1]) then
     full_path = full_path .. opts.fargs[1]
   else
     full_path = full_path .. os.date("%A_%B_%d_%Y_%I_%M_%S_%p") .. ".md"
